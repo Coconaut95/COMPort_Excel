8241551a6f7190d2c05bbafa22d695db66b95ec1
facundo <coconaut95@gmail.com>
2022-09-01 07:15:14 -0300

    More changes

--- com_serie.py
+++ com_serie.py
@@ -23,25 +23,23 @@ if not os.path.exists(full_path):
 #     print(p)
 
 def read_serial_port():
-
+    
     ports = list(port_lists.comports())
     for p in ports:
         print(p)
 
-    port_com = input("Ingrese Puerto de comunicacion: ")
+    port_com = input("\nIngrese Puerto de comunicacion: ")
     ser = serial.Serial(port_com, 115200)
     ser.setDTR(False) # Data Terminal Ready deshabilitado
     sleep(1)
     ser.flushInput()
-    ser.setDTR(True)
-    # start = time.time()
+    ser.setDTR(True)    
     dato = []
     cadena = " "
 
     while True:
         dato = ser.readline()
-        dato = dato.decode('UTF-8')      # Por defecto se codifica en byte. Lo convierto a string
-        # print(dato)
+        dato = dato.decode('UTF-8')      # Por defecto se codifica en byte. Lo convierto a string        
         cadena = cadena + dato           # concatena cada linea.
         subcadena = cadena[-6:]          # Verifica los ultimos 6 caracteres de la cadena.
         if subcadena == "\r\n\r\n\r\n":
